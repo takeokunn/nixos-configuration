69b79192d5eb43a8308af60c3a40e587774bd951
takeokunn <bararararatty@gmail.com>
2024-06-09 02:47:35 +0900

    Update

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,107 +1,136 @@
 { config, pkgs, ... }: {
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Asia/Tokyo";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ja_JP.UTF-8";
-    LC_IDENTIFICATION = "ja_JP.UTF-8";
-    LC_MEASUREMENT = "ja_JP.UTF-8";
-    LC_MONETARY = "ja_JP.UTF-8";
-    LC_NAME = "ja_JP.UTF-8";
-    LC_NUMERIC = "ja_JP.UTF-8";
-    LC_PAPER = "ja_JP.UTF-8";
-    LC_TELEPHONE = "ja_JP.UTF-8";
-    LC_TIME = "ja_JP.UTF-8";
+  boot = {
+    hardwareScan = true;
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "jp";
-    xkb.variant = "";
-    xkb.options = "ctrl:swapcaps";
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+    enableIPv6 = true;
+    wireless.enable = true;
   };
 
-  # Configure console keymap
-  console.keyMap = "jp106";
+  time = { timeZone = "Asia/Tokyo"; };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "ja_JP.UTF-8";
+      LC_IDENTIFICATION = "ja_JP.UTF-8";
+      LC_MEASUREMENT = "ja_JP.UTF-8";
+      LC_MONETARY = "ja_JP.UTF-8";
+      LC_NAME = "ja_JP.UTF-8";
+      LC_NUMERIC = "ja_JP.UTF-8";
+      LC_PAPER = "ja_JP.UTF-8";
+      LC_TELEPHONE = "ja_JP.UTF-8";
+      LC_TIME = "ja_JP.UTF-8";
+    };
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        gdm.enable = true;
+        gnome.enable = true;
+      };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.take = {
-    isNormalUser = true;
-    description = "take";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs;
-      [
-        firefox
-        #  thunderbird
-      ];
+      xkb = {
+        layout = "jp";
+        variant = "";
+        options = "ctrl:swapcaps";
+      };
+
+    };
+
+    tlp.enable = true;
+    printing.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      media-session.enable = true;
+    };
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  console = { keyMap = "jp106"; };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.fish.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
+  sound = { enable = true; };
+
+  hardware = {
+    opengl.enable = true;
+    bluetooth.enable = true;
   };
 
-  # List services that you want to enable:
+  security = {
+    rtkit.enable = true;
+    tpm2.enable = true;
+  };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  users.users = {
+    take = {
+      isNormalUser = true;
+      extraGroups = [ "networkmanager" "wheel" ];
+      shell = pkgs.fish;
+      useDefaultShell = true;
+      packages = with pkgs; [ firefox google-chrome fcitx5-skk ];
+    };
+  };
+
+  nix = {
+    gc.automatic = true;
+    gc.options = "--delete-older-than 5d";
+  };
+  nixpkgs = { config.allowUnfree = true; };
+
+  programs = {
+    fish.enable = true;
+
+    gnupg = {
+      enable = true;
+      agent = { enable = true; };
+    };
+
+    google-chrome = {
+      enable = true;
+      commandLineArgs =
+        [ "--enable-features=UseOzonePlatfor" "--ozone-platform=x11" ];
+    };
+
+    # sway = {
+    #   enable = true;
+    #   xwayland.enable = true;
+    # };
+    # waybar.enable = true;
+  };
+
+  system = {
+    stateVersion = "23.11";
+
+    # MEMO: 有効にしたい
+    autoUpgrade.enable = false;
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [
+      freefont_ttf
+      gyre-fonts
+      liberation_ttf
+      migu
+      nerdfonts
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      twemoji-color-font
+      unifont
+      dejavu_fonts
+    ];
+  };
 }
