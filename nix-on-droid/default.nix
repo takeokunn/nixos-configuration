67a670736aa8ded8d5f9e0a2d745f529965b88c1
takeokunn <bararararatty@gmail.com>
2024-06-13 01:03:01 +0900

    update

--- nix-on-droid/default.nix
+++ nix-on-droid/default.nix
@@ -1,6 +1,71 @@
 { config, lib, pkgs, ... }: {
   environment = {
-    packages = import ../home-manager/packages/basic.nix pkgs;
+    packages = with pkgs; [
+      # for terminal tools
+      bat
+      bottom
+      devbox
+      direnv
+      du-dust
+      extract_url
+      eza
+      fd
+      gnupg
+      nkf
+      offlineimap
+      openssl
+      peco
+      pv
+      ripgrep
+      rlwrap
+      tmux
+      tree
+      unixtools.procps
+      unixtools.watch
+      wget
+
+      # for query
+      csvq
+      jq
+      yq
+
+      # for git
+      ghq
+      git
+      tig
+
+      # for cloud tools
+      awscli
+
+      # for network tools
+      speedtest-cli
+
+      # for password tools
+      pwgen
+      (pass.withExtensions (extensions: with extensions; [ pass-otp ]))
+
+      # for editor
+      editorconfig-core-c
+      tree-sitter
+      (tree-sitter.withPlugins (p: builtins.attrValues p))
+      neovim
+
+      # for shell
+      fish
+
+      # for DB
+      mysql
+
+      # for jokes
+      asciiquarium
+      cmatrix
+      fastfetch
+      genact
+      sl
+
+      # for ai
+      ollama
+    ];
     etcBackupExtension = ".bak";
   };
 
