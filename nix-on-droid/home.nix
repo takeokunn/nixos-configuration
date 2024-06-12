cb615d2c4080a3b13fa95c0c10f1a8e0c02290a6
takeokunn <bararararatty@gmail.com>
2024-06-12 23:49:28 +0900

    update

--- nix-on-droid/home.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, pkgs }: {
-  home = {
-    stateVersion = "24.11";
-    packages = [ pkgs.hello ];
-  };
-}
--- systems/OPPO-A79/default.nix
+++ systems/OPPO-A79/default.nix
@@ -1,8 +1,5 @@
-{ self, nixpkgs, nix-on-droid, emacs-overlay }:
-let system = "x86_64";
-in {
+{ self, nixpkgs, nix-on-droid }: {
   OPPO-A79 = nix-on-droid.lib.nixOnDroidConfiguration {
     modules = [ ../../nix-on-droid ];
-    extraSpecialArgs = { inherit system nixpkgs emacs-overlay; };
   };
 }
