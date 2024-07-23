56b7549622272780dcbd3419979b19d1afe1263a
takeokunn <bararararatty@gmail.com>
2024-07-23 14:31:32 +0900

    update

--- home-manager/programs/password-store/default.nix
+++ home-manager/programs/password-store/default.nix
@@ -3,8 +3,10 @@
     enable = true;
     package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
     settings = {
-      PASSWORD_STORE_DIR = "~/ghq/github.com/takeokunn/password-store";
+      PASSWORD_STORE_DIR =
+        "$HOME/ghq/github.com/takeokunn/private/password-store";
       PASSWORD_STORE_ENABLE_EXTENSIONS = "true";
+      PASSWORD_STORE_CLIP_TIME = "60";
     };
   };
 }
