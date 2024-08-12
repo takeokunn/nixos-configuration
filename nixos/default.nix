b8cf1ff837869ff3a2366a4d7fa88f4c7c794650
takeokunn <bararararatty@gmail.com>
2024-08-12 14:55:35 +0900

    update

--- nixos/default.nix
+++ nixos/default.nix
@@ -102,6 +102,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    emacs
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
