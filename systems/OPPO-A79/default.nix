24949350e7ef532cb55432512f78c87c3f7d574c
takeokunn <bararararatty@gmail.com>
2024-08-14 14:46:25 +0900

    update

--- systems/OPPO-A79/default.nix
+++ systems/OPPO-A79/default.nix
@@ -1,4 +1,4 @@
-{ self, nixpkgs, emacs-overlay, nix-on-droid }:
+{ self, nixpkgs, home-manager, emacs-overlay, nix-on-droid }:
 let system = "aarch64-linux";
 in {
   OPPO-A79 = nix-on-droid.lib.nixOnDroidConfiguration {
