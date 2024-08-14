1bd970a1898b1f80da36d0ac6853ac6c329f72d2
takeokunn <bararararatty@gmail.com>
2024-08-14 15:51:34 +0900

    update

--- home-manager/programs/minimum.nix
+++ home-manager/programs/minimum.nix
@@ -1,40 +1,40 @@
 { pkgs }:
 let
-  # bat = import ./bat { inherit pkgs; };
-  # bottom = import ./bottom;
-  # direnv = import ./direnv;
-  # editorconfig = import ./editorconfig { inherit pkgs; };
-  # eza = import ./eza;
-  # fish = import ./fish { inherit pkgs; };
-  # git = import ./git;
-  # gnupg = import ./gnupg;
-  # misc = import ./misc;
-  # neovim = import ./neovim;
-  # password-store = import ./password-store { inherit pkgs; };
-  # peco = import ./peco { inherit pkgs; };
-  # readline = import ./readline;
-  # ripgrep = import ./ripgrep;
-  # tig = import ./tig { inherit pkgs; };
-  # tmux = import ./tmux { inherit pkgs; };
-  # vim = import ./vim;
+  bat = import ./bat { inherit pkgs; };
+  bottom = import ./bottom;
+  direnv = import ./direnv;
+  editorconfig = import ./editorconfig { inherit pkgs; };
+  eza = import ./eza;
+  fish = import ./fish { inherit pkgs; };
+  git = import ./git;
+  gnupg = import ./gnupg;
+  misc = import ./misc;
+  neovim = import ./neovim;
+  password-store = import ./password-store { inherit pkgs; };
+  peco = import ./peco { inherit pkgs; };
+  readline = import ./readline;
+  ripgrep = import ./ripgrep;
+  tig = import ./tig { inherit pkgs; };
+  tmux = import ./tmux { inherit pkgs; };
+  vim = import ./vim;
   wget = import ./wget { inherit pkgs; };
 in [
-  # bat
-  # bottom
-  # direnv
-  # editorconfig
-  # eza
-  # fish
-  # git
-  # gnupg
-  # misc
-  # neovim
-  # password-store
-  # peco
-  # readline
-  # ripgrep
-  # tig
-  # tmux
-  # vim
+  bat
+  bottom
+  direnv
+  editorconfig
+  eza
+  fish
+  git
+  gnupg
+  misc
+  neovim
+  password-store
+  peco
+  readline
+  ripgrep
+  tig
+  tmux
+  vim
   wget
 ]
--- nix-on-droid/default.nix
+++ nix-on-droid/default.nix
@@ -2,7 +2,6 @@
   time.timeZone = "Asia/Tokyo";
 
   system.stateVersion = "24.05";
-  environment.packages = with pkgs; [ git vim ];
 
   nix.extraOptions = ''
     experimental-features = nix-command flakes
