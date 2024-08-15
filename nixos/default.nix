5c5b663e87c6198573fe6bec7b3b7fee8116c05b
takeokunn <bararararatty@gmail.com>
2024-08-15 23:55:01 +0900

    update

--- nixos/default.nix
+++ nixos/default.nix
@@ -23,7 +23,7 @@
   time.timeZone = "Asia/Tokyo";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "ja_JP.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "ja_JP.UTF-8";
@@ -43,10 +43,10 @@
       displayManager.gdm.enable = true;
       desktopManager.gnome.enable = true;
 
-      xkb = {
-        layout = "jp";
-        model = "pc106";
-      };
+      # xkb = {
+      #   layout = "jp";
+      #   model = "pc106";
+      # };
     };
   };
 
