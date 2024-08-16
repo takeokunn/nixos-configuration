8eb62ff5c08c91167e46dab3a2c7e0a95b03e62c
takeokunn <bararararatty@gmail.com>
2024-08-17 03:24:45 +0900

    update

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -10,14 +10,6 @@
     user = username;
   };
 
-  pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
   xremap = {
     userName = username;
     serviceMode = "system";
