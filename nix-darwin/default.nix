b8bd54bb7325413b9ceefae6bc1f1a08e318705f
takeokunn <bararararatty@gmail.com>
2024-08-17 16:42:38 +0900

    Refactor nix-darwin/default.nix

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -2,20 +2,14 @@
 let
   nix = import ./modules/nix.nix;
   fonts = import ./modules/fonts.nix { inherit pkgs; };
-  service = import ./modules/service.nix;
+  services = import ./modules/services.nix;
   system = import ./modules/system.nix;
   homebrew = import ./modules/homebrew.nix;
   networking = import ./modules/networking.nix;
   security = import ./modules/security.nix { inherit username; };
   launchd = import ./modules/launchd.nix { inherit pkgs; };
 in {
+  inherit nix services fonts system homebrew networking security launchd;
+
   environment.shells = with pkgs; [ fish ];
-  nix = nix;
-  services = service;
-  fonts = fonts;
-  system = system;
-  homebrew = homebrew;
-  networking = networking;
-  security = security;
-  launchd = launchd;
 }
