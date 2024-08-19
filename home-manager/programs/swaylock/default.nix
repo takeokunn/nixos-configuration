104a796932388cbc86eebaa29cc2d96f86359745
takeokunn <bararararatty@gmail.com>
2024-08-20 02:25:33 +0900

    update

--- home-manager/programs/swaylock/default.nix
+++ home-manager/programs/swaylock/default.nix
@@ -1,6 +1,3 @@
 {
-  home.file = {
-    ".config/swaylock/config".source = ./config;
-    ".config/swaylock/dracula-wallpaper.svg".source = ./dracula-wallpaper.svg;
-  };
+  home.file.".config/swaylock/config".source = ./config;
 }
