312e28ad179b0d9b15393d0ba16737e124d03743
takeokunn <bararararatty@gmail.com>
2024-08-20 01:30:58 +0900

    Update

--- home-manager/programs/waybar/default.nix
+++ home-manager/programs/waybar/default.nix
@@ -3,6 +3,5 @@
     ".config/waybar/colors.css".source = ./colors.css;
     ".config/waybar/style.css".source = ./style.css;
     ".config/waybar/config".source = ./config;
-    ".config/waybar/wittr.sh".source = ./wittr.sh;
   };
 }
--- home-manager/programs/waybar/wittr.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-req=$(curl -s wttr.in/CITY?format="%t|%l+(%c%f)+%h,+%C")
-bar=$(echo $req | awk -F "|" '{print $1}')
-tooltip=$(echo $req | awk -F "|" '{print $2}')
-echo "{\"text\":\"$bar\", \"tooltip\":\"$tooltip\"}"
