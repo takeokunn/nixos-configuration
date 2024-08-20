9e13d22b955335fd62e01c80447e9c048d4d72bc
takeokunn <bararararatty@gmail.com>
2024-08-20 17:49:30 +0900

    update

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -1,4 +1,8 @@
 { username }: {
+  ntp.enable = true;
+  ollama.enable = true;
+  offlineimap.enable = true;
+
   xserver = {
     enable = true;
     xkb.layout = "jp";
@@ -31,22 +35,32 @@
     };
   };
 
-  ntp.enable = true;
-
-  ollama = {
-    enable = true;
-    models = "gemma2:27b";
-  };
-
   tlp = {
     enable = true;
     settings = {
-      AHCI_RUNTIME_PM_ON_AC = "auto";
-      AHCI_RUNTIME_PM_ON_BAT = "auto";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
       RUNTIME_PM_ON_AC = "auto";
       RUNTIME_PM_ON_BAT = "auto";
+
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+
+      SATA_LINKPWR_ON_AC = "max_performance";
+      SATA_LINKPWR_ON_BAT = "min_power";
+
+      SOUND_POWER_SAVE_ON_AC = 0;
+      SOUND_POWER_SAVE_ON_BAT = 1;
     };
   };
-
-  offlineimap.enable = true;
 }
