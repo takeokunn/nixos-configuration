19ea1e3ef023713b8e8750ed85c1f7b01acb890c
takeokunn <bararararatty@gmail.com>
2024-09-02 00:23:21 +0900

    Update

--- home-manager/services/emacs/default.nix
+++ home-manager/services/emacs/default.nix
@@ -1,6 +1,7 @@
 { pkgs, emacsPkgs }: {
   services.emacs = {
-    enable = pkgs.stdenv.isLinux;
+    # enable = pkgs.stdenv.isLinux;
+    enable = false;
     package = emacsPkgs;
     defaultEditor = true;
   };
