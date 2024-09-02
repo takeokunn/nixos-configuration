fcffefa7c06793f1959bfc3736d3c51a7fdb4d47
takeokunn <bararararatty@gmail.com>
2024-09-03 00:25:01 +0900

    update

--- home-manager/modules/peco.nix
+++ home-manager/modules/peco.nix
@@ -1,14 +1,17 @@
 { pkgs, lib, config, ... }:
-let cfg = config.programs.peco;
+let
+  cfg = config.programs.peco;
+  jsonFormat = pkgs.formats.json { };
 in with lib; {
   options.programs.peco = {
     enable = mkEnableOption "Simplistic interactive filtering tool";
     package = mkPackageOption pkgs "peco" { };
-    config = mkOption { type = types.lines; };
+    config = mkOption { type = jsonFormat.type; };
   };
 
   config = mkIf cfg.enable {
     home.packages = [ cfg.package ];
-    xdg.configFile."peco/config.json".text = cfg.config;
+    xdg.configFile."peco/config.json".source =
+      jsonFormat.generate "config.json" cfg.config;
   };
 }
--- home-manager/programs/peco/default.nix
+++ home-manager/programs/peco/default.nix
@@ -1,18 +1,16 @@
 { pkgs }: {
   programs.peco = {
     enable = true;
-    config = ''
-      {
-          "Use256Color": true,
-          "StickySelection": true,
-          "Prompt": "[peco] >",
-          "Keymap": {
-              "C-k": "peco.KillEndOfLine",
-              "M-v": "peco.ScrollPageUp",
-              "C-v": "peco.ScrollPageDown",
-              "C-x,C-c": "peco.Cancel"
-          }
-      }
-    '';
+    config = {
+      Use256Color = true;
+      StickySelection = true;
+      Prompt = "[peco] >";
+      Keymap = {
+        "C-k" = "peco.KillEndOfLine";
+        "M-v" = "peco.ScrollPageUp";
+        "C-v" = "peco.ScrollPageDown";
+        "C-x,C-c" = "peco.Cancel";
+      };
+    };
   };
 }
