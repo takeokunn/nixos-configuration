b193e1f97337a02e97c1b787fce2cc53d2f8de8b
takeokunn <bararararatty@gmail.com>
2024-09-05 09:03:56 +0900

    update

--- home-manager/programs/bat/default.nix
+++ home-manager/programs/bat/default.nix
@@ -1,25 +1,17 @@
-{ pkgs }: {
+{ pkgs }:
+let sources = pkgs.callPackage ../../../_sources/generated.nix { };
+in {
   programs.bat = {
     enable = true;
     themes = {
       dracula = {
-        src = pkgs.fetchFromGitHub {
-          owner = "dracula";
-          repo = "sublime";
-          rev = "456d3289827964a6cb503a3b0a6448f4326f291b";
-          sha256 = "1flajlii1w40g2yqs5iyfjvqxah1d83mvglsid2kn1mbajyshq7j";
-        };
+        src = sources.dracula-sublime.src;
         file = "Dracula.tmTheme";
       };
     };
     syntaxes = {
       gleam = {
-        src = pkgs.fetchFromGitHub {
-          owner = "molnarmark";
-          repo = "sublime-gleam";
-          rev = "ff9638511e05b0aca236d63071c621977cffce38";
-          sha256 = "1fmxd95y15ay57zww9prkzd6kvx6bdlwrwk47law7rbb7xksi2gp";
-        };
+        src = sources.sublime-gleam.src;
         file = "syntax/gleam.sublime-syntax";
       };
     };
