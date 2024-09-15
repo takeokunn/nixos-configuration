af59132d869cd65ef9b6d1b6306dab26a11fffe1
takeokunn <bararararatty@gmail.com>
2024-09-15 15:30:48 +0900

    Update

---
+++ script.el
@@ -0,0 +1,12 @@
+(require 'ox-publish)
+
+(defun export-org-files ()
+  "Exports Org files to html."
+  (interactive)
+  (let ((org-html-htmlize-output-type 'css)
+        (org-publish-project-alist `(("org"
+                                      :base-directory "./home-manager/programs/emacs/elisp/"
+                                      :base-extension "org"
+                                      :exclude "README.org"
+                                      :publishing-directory "public/"))))
+    (org-publish-all t)))
