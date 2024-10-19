9dd7bde4c1d22bef1b73a517509fced35ac628b9
takeokunn <bararararatty@gmail.com>
2024-10-19 11:30:40 +0900

    Update

--- home-manager/programs/neovim/plugins/skk/default.nix
+++ home-manager/programs/neovim/plugins/skk/default.nix
@@ -5,8 +5,5 @@ in [{
   plugin = plugins.skkeleton;
   config = ''
     vim.keymap.set({ 'i', 'c' }, '<C-j>', '<Plug>(skkeleton-toggle)', { silent = true })
-    vim.fn['skkeleton#config']({
-      globalDictionaries = { "${pkgs.skk-dicts}//share/SKK-JISYO.L" }
-    })
   '';
 }]
