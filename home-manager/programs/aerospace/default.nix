012500dc5c7ea3163c57b2a331cb6ecedda29812
takeokunn <bararararatty@gmail.com>
2024-11-12 14:25:57 +0900

    Update

--- home-manager/programs/aerospace/default.nix
+++ home-manager/programs/aerospace/default.nix
@@ -29,67 +29,67 @@
       mode = {
         main = {
           binding = {
-            "alt-h" = "focus left";
-            "alt-l" = "focus right";
+            alt-h = "focus left";
+            alt-l = "focus right";
 
-            "alt-shift-h" = "move left";
-            "alt-shift-l" = "move right";
+            alt-shift-h = "move left";
+            alt-shift-l = "move right";
 
-            "alt-shift-space" = "layout floating tiling";
+            alt-shift-space = "layout floating tiling";
 
-            "alt-1" = "workspace 1";
-            "alt-2" = "workspace 2";
-            "alt-3" = "workspace 3";
-            "alt-4" = "workspace 4";
-            "alt-5" = "workspace 5";
-            "alt-6" = "workspace 6";
-            "alt-7" = "workspace 7";
-            "alt-8" = "workspace 8";
-            "alt-9" = "workspace 9";
-            "alt-0" = "workspace 10";
+            alt-1 = "workspace 1";
+            alt-2 = "workspace 2";
+            alt-3 = "workspace 3";
+            alt-4 = "workspace 4";
+            alt-5 = "workspace 5";
+            alt-6 = "workspace 6";
+            alt-7 = "workspace 7";
+            alt-8 = "workspace 8";
+            alt-9 = "workspace 9";
+            alt-0 = "workspace 10";
 
-            "alt-shift-1" = [
+            alt-shift-1 = [
               "move-node-to-workspace 1"
               "workspace 1"
             ];
-            "alt-shift-2" = [
+            alt-shift-2 = [
               "move-node-to-workspace 2"
               "workspace 2"
             ];
-            "alt-shift-3" = [
+            alt-shift-3 = [
               "move-node-to-workspace 3"
               "workspace 3"
             ];
-            "alt-shift-4" = [
+            alt-shift-4 = [
               "move-node-to-workspace 4"
               "workspace 4"
             ];
-            "alt-shift-5" = [
+            alt-shift-5 = [
               "move-node-to-workspace 5"
               "workspace 5"
             ];
-            "alt-shift-6" = [
+            alt-shift-6 = [
               "move-node-to-workspace 6"
               "workspace 6"
             ];
-            "alt-shift-7" = [
+            alt-shift-7 = [
               "move-node-to-workspace 7"
               "workspace 7"
             ];
-            "alt-shift-8" = [
+            alt-shift-8 = [
               "move-node-to-workspace 8"
               "workspace 8"
             ];
-            "alt-shift-9" = [
+            alt-shift-9 = [
               "move-node-to-workspace 9"
               "workspace 9"
             ];
-            "alt-shift-0" = [
+            alt-shift-0 = [
               "move-node-to-workspace 10"
               "workspace 10"
             ];
 
-            "alt-r" = "mode resize";
+            alt-r = "mode resize";
           };
         };
 
