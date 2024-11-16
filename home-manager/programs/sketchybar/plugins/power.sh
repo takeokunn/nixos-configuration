9eebc37fcb902b42d24b5a1950983e2bf3f85452
takeokunn <bararararatty@gmail.com>
2024-11-16 14:35:48 +0900

    Update

--- home-manager/programs/sketchybar/plugins/power.sh
+++ home-manager/programs/sketchybar/plugins/power.sh
@@ -4,31 +4,41 @@ PERCENTAGE=$(pmset -g batt | grep -Eo "[0-9]+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
 case ${PERCENTAGE} in
-    100) ICON="󰁹"
-    ;;
-    9[0-9]) ICON="󰁹"
-    ;;
-    8[0-9]) ICON="󰂂"
-    ;;
-    7[0-9]) ICON="󰂁"
-    ;;
-    6[0-9]) ICON="󰂀"
-    ;;
-    5[0-9]) ICON="󰁿"
-    ;;
-    4[0-9]) ICON="󰁾"
-    ;;
-    3[0-9]) ICON="󰁽"
-    ;;
-    2[0-9]) ICON="󰁼"
-    ;;
-    1[0-9]) ICON="󰁻"
-    ;;
-    0[0-9]) ICON="󰁺"
-    ;;
-    *) ICON="󰂎"
+100)
+  ICON="󰁹"
+  ;;
+9[0-9])
+  ICON="󰁹"
+  ;;
+8[0-9])
+  ICON="󰂂"
+  ;;
+7[0-9])
+  ICON="󰂁"
+  ;;
+6[0-9])
+  ICON="󰂀"
+  ;;
+5[0-9])
+  ICON="󰁿"
+  ;;
+4[0-9])
+  ICON="󰁾"
+  ;;
+3[0-9])
+  ICON="󰁽"
+  ;;
+2[0-9])
+  ICON="󰁼"
+  ;;
+1[0-9])
+  ICON="󰁻"
+  ;;
+0[0-9])
+  ICON="󰁺"
+  ;;
+*) ICON="󰂎" ;;
 esac
-
 
 if [[ $CHARGING != "" ]]; then
   ICON=""
