7f8aff18bb84b1a3b5ffcb254417b09464bf7b0c
takeokunn <bararararatty@gmail.com>
2024-11-29 16:49:58 +0900

    Update

--- home-manager/programs/fish/default.nix
+++ home-manager/programs/fish/default.nix
@@ -30,9 +30,6 @@
       set -x TMPDIR /tmp
       set -x COLORTERM truecolor
 
-      # for nix
-      set -x NIX_CONFIG "access-tokens = github.com="(gh auth token)
-
       if test -d /etc/profiles/per-user/(whoami)/bin
         fish_add_path /etc/profiles/per-user/(whoami)/bin
       end
--- home-manager/programs/gh/default.nix
+++ home-manager/programs/gh/default.nix
@@ -10,6 +10,9 @@
   programs.fish = {
     interactiveShellInit = ''
       eval (gh completion -s fish| source)
+
+      # for nix
+      set -x NIX_CONFIG "access-tokens = github.com="(gh auth token)
     '';
   };
 }
