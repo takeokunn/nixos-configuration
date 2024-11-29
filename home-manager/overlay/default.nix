b574f6b767e176405622d74a28b7cb6f364b0a81
takeokunn <bararararatty@gmail.com>
2024-11-29 15:05:17 +0900

    Fix lint

--- home-manager/overlay/default.nix
+++ home-manager/overlay/default.nix
@@ -1,18 +1,20 @@
 { emacs-overlay }:
 let
-  lnav-overlay = (self: super: {
-    lnav = super.lnav.overrideAttrs (oldAttrs: {
-      buildInputs = with self; [
-        bzip2
-        ncurses
-        pcre2
-        readline
-        sqlite
-        curl
-        libarchive
-      ];
-    });
-  });
+  lnav-overlay = (
+    self: super: {
+      lnav = super.lnav.overrideAttrs (oldAttrs: {
+        buildInputs = with self; [
+          bzip2
+          ncurses
+          pcre2
+          readline
+          sqlite
+          curl
+          libarchive
+        ];
+      });
+    }
+  );
 
 in
 [
