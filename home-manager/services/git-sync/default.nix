1ae80e2a2c668345b60ea0e3da62f05a5cb660a2
takeokunn <bararararatty@gmail.com>
2024-12-06 19:00:29 +0900

    Update

--- home-manager/services/git-sync/default.nix
+++ home-manager/services/git-sync/default.nix
@@ -1,12 +1,12 @@
 { config, ... }:
 {
   services.git-sync = {
-    enable = true;
+    enable = false;
     repositories = {
       private = {
         uri = "git@github.com:takeokunn/private";
         path = "${config.home.homeDirectory}/ghq/github.com/takeokunn/private";
-        interval = 600;
+        interval = 60 * 60 * 3;
       };
     };
   };
