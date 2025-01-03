b5a4995fd40c4965272283eb27b0fc7d0e675032
takeokunn <bararararatty@gmail.com>
2025-01-03 16:57:34 +0900

    Update

--- home-manager/programs/emacs/epkgs/packages/ai/packages.nix
+++ home-manager/programs/emacs/epkgs/packages/ai/packages.nix
@@ -13,6 +13,21 @@
     ignoreCompilationError = false;
   };
 
+  copilot-chat = epkgs.melpaBuild {
+    pname = "copilot-chat";
+    version = "0.0.1";
+    src = sources.emacs-copilot-chat.src;
+
+    packageRequires = with epkgs; [
+      request
+      markdown-mode
+      chatgpt-shell
+      magit
+    ];
+
+    ignoreCompilationError = false;
+  };
+
   plz-media-type = epkgs.melpaBuild {
     pname = "plz-media-type";
     version = "0.0.1";
@@ -45,21 +60,6 @@
       plz
       plz-media-type
       plz-event-source
-    ];
-
-    ignoreCompilationError = false;
-  };
-
-  copilot-chat = epkgs.melpaBuild {
-    pname = "copilot-chat";
-    version = "0.0.1";
-    src = sources.emacs-copilot-chat.src;
-
-    packageRequires = with epkgs; [
-      request
-      markdown-mode
-      chatgpt-shell
-      magit
     ];
 
     ignoreCompilationError = false;
