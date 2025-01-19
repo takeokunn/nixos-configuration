8d22fc6f0e5b32c37ee10586ef9ced49bf453280
takeokunn <bararararatty@gmail.com>
2025-01-19 11:28:55 +0900

    Update

--- home-manager/overlay/basic.nix
+++ home-manager/overlay/basic.nix
@@ -1,23 +1,4 @@
 { emacs-overlay }:
-let
-  lnav-overlay = (
-    self: super: {
-      lnav = super.lnav.overrideAttrs (oldAttrs: {
-        buildInputs = with self; [
-          bzip2
-          ncurses
-          pcre2
-          readline
-          sqlite
-          curl
-          libarchive
-        ];
-      });
-    }
-  );
-
-in
 [
-  lnav-overlay
   (import emacs-overlay)
 ]
