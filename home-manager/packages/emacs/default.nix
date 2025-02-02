16c55223327058cd184aa189b2b324dd1f823463
takeokunn <bararararatty@gmail.com>
2025-02-02 23:39:38 +0900

    Add dap-mode overlay

--- home-manager/packages/emacs/default.nix
+++ home-manager/packages/emacs/default.nix
@@ -2,22 +2,32 @@
   pkgs,
   sources,
 }:
+let
+  override = final: prev: {
+    dap-mode = prev.melpaPackages.dap-mode.overrideAttrs (old: {
+      preBuild = null;
+    });
+  };
+in
 {
   emacs-git = pkgs.emacsWithPackagesFromUsePackage {
     config = builtins.toFile "empty.el" "";
     package = pkgs.emacs-git;
     extraEmacsPackages = import ./epkgs { inherit pkgs sources; };
+    override = override;
   };
 
   emacs-unstable = pkgs.emacsWithPackagesFromUsePackage {
     config = builtins.toFile "empty.el" "";
     package = pkgs.emacs-unstable;
     extraEmacsPackages = import ./epkgs { inherit pkgs sources; };
+    override = override;
   };
 
   emacs-stable = pkgs.emacsWithPackagesFromUsePackage {
     config = builtins.toFile "empty.el" "";
     package = pkgs.emacs;
     extraEmacsPackages = import ./epkgs { inherit pkgs sources; };
+    override = override;
   };
 }
