6e48bf94f4c5ba1de591a1ba468e0be74d17c3c7
takeokunn <bararararatty@gmail.com>
2025-03-05 17:51:55 +0900

    chore(neovim): comment out orgmode plugin configuration in default.nix

--- home-manager/programs/neovim/plugins/language/default.nix
+++ home-manager/programs/neovim/plugins/language/default.nix
@@ -2,14 +2,14 @@
 with pkgs.vimPlugins;
 [
   vim-markdown
-  {
-    type = "lua";
-    plugin = orgmode;
-    config = ''
-      require('orgmode').setup({
-        org_agenda_files = '~/ghq/github.com/takeokunn/private/**/*.org',
-        org_default_notes_file = '~/ghq/github.com/takeokunn/private/memo.org',
-      })
-    '';
-  }
+  # {
+  #   type = "lua";
+  #   plugin = orgmode;
+  #   config = ''
+  #     require('orgmode').setup({
+  #       org_agenda_files = '~/ghq/github.com/takeokunn/private/**/*.org',
+  #       org_default_notes_file = '~/ghq/github.com/takeokunn/private/memo.org',
+  #     })
+  #   '';
+  # }
 ]
