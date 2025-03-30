aa9ef56b202447f3bf9dd09a799a3219c049e49f
takeokunn <bararararatty@gmail.com>
2025-03-31 03:20:02 +0900

    fix(networking): remove hardcoded DNS entries from configuration

--- nix-darwin/config/networking.nix
+++ nix-darwin/config/networking.nix
@@ -5,11 +5,5 @@
       "Ethernet Adaptor"
       "Thunderbolt Ethernet"
     ];
-    dns = [
-      "45.90.28.125"
-      "45.90.30.125"
-      "2a07:a8c0::2c:6735"
-      "2a07:a8c1::2c:6735"
-    ];
   };
 }
