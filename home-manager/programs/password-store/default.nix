f4f00e39d2b1bf42986f0177688858f98278c813
takeokunn <bararararatty@gmail.com>
2025-05-10 14:52:49 +0900

    fix(password-store): update PASSWORD_STORE_DIR to include 'current' directory

--- home-manager/programs/password-store/default.nix
+++ home-manager/programs/password-store/default.nix
@@ -4,7 +4,7 @@
     enable = true;
     package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
     settings = {
-      PASSWORD_STORE_DIR = "$HOME/ghq/github.com/takeokunn/private/password-store";
+      PASSWORD_STORE_DIR = "$HOME/ghq/github.com/takeokunn/private/password-store/current";
       PASSWORD_STORE_ENABLE_EXTENSIONS = "true";
       PASSWORD_STORE_CLIP_TIME = "60";
     };
