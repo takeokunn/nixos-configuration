1542ca6d512606ac779a8bbd9828d5af8e064374
takeokunn <bararararatty@gmail.com>
2025-05-26 14:44:22 +0900

    feat(home-manager): update zellij configuration with new settings
    Updated the zellij configuration to include additional settings such as theme, default layout, simplified UI, and fish shell integration. Disabled mouse mode and startup tips while enabling session attachment and shell exit behavior.

--- home-manager/programs/zellij/default.nix
+++ home-manager/programs/zellij/default.nix
@@ -1,5 +1,18 @@
 {
-  programs.zelliji = {
-    enable = true;
+  programs.zellij = {
+    enable = false;
+    attachExistingSession = true;
+    exitShellOnExit = true;
+    enableFishIntegration = true;
+    settings = {
+      theme = "dracula";
+      default_layout = "compact";
+      simplified_ui = true;
+      default_shell = "fish";
+      mouse_mode = false;
+      scroll_buffer_size = 100000;
+      show_startup_tips = false;
+      show_release_notes = false;
+    };
   };
 }
