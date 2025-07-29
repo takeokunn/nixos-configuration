132f2b4a8dbe3bae53c621f6d93edc7b31baa227
takeokunn <bararararatty@gmail.com>
2025-07-30 00:26:07 +0900

    feat(nixos): add overlay for stdenv with mold linker

--- nixos/config/nix.nix
+++ nixos/config/nix.nix
@@ -3,4 +3,10 @@
     settings.cores = 8;
     optimise.automatic = true;
   };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      stdenv = prev.stdenvAdapters.useMold prev.stdenv;
+    })
+  ];
 }
