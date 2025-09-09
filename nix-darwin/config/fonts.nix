3a089cea35148c96047ef1663aaa06625170dea1
takeokunn <bararararatty@gmail.com>
2025-09-09 18:42:14 +0900

    update

--- nix-darwin/config/fonts.nix
+++ nix-darwin/config/fonts.nix
@@ -15,8 +15,5 @@
     emacs-all-the-icons-fonts
     font-awesome
     font-awesome_5
-    iosevka
-    (iosevka-bin.override { variant = "Aile"; })
-    (iosevka-bin.override { variant = "Etoile"; })
   ];
 }
