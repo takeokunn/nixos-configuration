2ed04d4161cf656d535e9533041761496be2a8c0
takeokunn <bararararatty@gmail.com>
2025-11-27 01:33:47 +0900

    update

--- nixos/config/programs.nix
+++ nixos/config/programs.nix
@@ -5,11 +5,6 @@
     noisetorch.enable = true;
     nix-ld.enable = true;
 
-    gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-    };
-
     sway = {
       enable = true;
       xwayland.enable = true;
