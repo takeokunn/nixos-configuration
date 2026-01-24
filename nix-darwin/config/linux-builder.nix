96b43322b384c56b9dfe6bc61902f12de5e219d2
takeokunn <bararararatty@gmail.com>
2026-01-24 20:20:54 +0900

    feat(config): disable sudo password and add builder to wheel group
    - Set security.sudo.wheelNeedsPassword to false for passwordless sudo
    - Add builder user to wheel group for elevated permissions

--- nix-darwin/config/linux-builder.nix
+++ nix-darwin/config/linux-builder.nix
@@ -13,6 +13,8 @@
         memorySize = lib.mkForce (1024 * 64);
         diskSize = lib.mkForce (1024 * 500);
       };
+      security.sudo.wheelNeedsPassword = false;
+      users.users.builder.extraGroups = [ "wheel" ];
       nix.settings = {
         experimental-features = [
           "nix-command"
