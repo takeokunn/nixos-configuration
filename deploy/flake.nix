4bbc6d84427675ba389f5fe0824d055f2da3da24
takeokunn <bararararatty@gmail.com>
2026-04-05 13:48:54 +0900

    fix: pre-fetch readthezero setup file for sandboxed Nix build
    The #+SETUPFILE HTTPS URL cannot be fetched in the Nix sandbox.
    Use pkgs.fetchurl to pre-fetch the setup file and replace the URL
    with the local store path during buildPhase.

--- deploy/flake.nix
+++ deploy/flake.nix
@@ -17,6 +17,10 @@
         emacsPkg = pkgs.emacs.override {
           withNativeCompilation = false;
         };
+        readthezeroSetup = pkgs.fetchurl {
+          url = "https://takeokunn.github.io/readthezero/readthezero-dracula.setup";
+          hash = "sha256-XJJXWu8YnYorhvb4iIoqztMOWpCjKk1HnqNaDNgXl4A=";
+        };
       in
       {
         packages = {
@@ -25,8 +29,12 @@
             src = ../.;
             nativeBuildInputs = with pkgs; [
               (emacsPkg.pkgs.withPackages (epkgs: with epkgs; [ htmlize ]))
+              perl
             ];
             buildPhase = ''
+              perl -pi -e "s|https://takeokunn.github.io/readthezero/readthezero-dracula.setup|${readthezeroSetup}|g" \
+                home-manager/programs/emacs/elisp/init.org \
+                home-manager/programs/emacs/elisp/early-init.org
               emacs --batch --load deploy/script.el --funcall export-org-files
             '';
             installPhase = ''
