eda209fc6000feea7dc56826c8c9d21a32d7471f
takeo obara <bararararatty+github-actions-gpg@gmail.com>
2026-04-26 20:13:08 +0000

    Update lockfile

--- home-manager/overlay/advanced.nix
+++ home-manager/overlay/advanced.nix
@@ -21,6 +21,8 @@
   })
   (final: prev: {
     # direnv test-zsh deadlocks on macOS due to /etc/zshenv inflating the env past the 64KB pipe buffer
-    direnv = prev.direnv.overrideAttrs (_: { doCheck = false; });
+    direnv = prev.direnv.overrideAttrs (_: {
+      doCheck = false;
+    });
   })
 ]
