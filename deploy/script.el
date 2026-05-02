43bb84edd16b2d255c328246422c7256d97c2f91
takeokunn <bararararatty@gmail.com>
2026-05-02 21:19:01 +0900

    fix(deploy): update emacs org-publish paths after concern reorganization
    script.el still referenced programs/emacs/ which was moved to editor/emacs/.

--- deploy/script.el
+++ deploy/script.el
@@ -8,12 +8,12 @@
   (interactive)
   (let ((org-html-htmlize-output-type 'css)
         (org-publish-project-alist `(("elisp"
-                                      :base-directory "./home-manager/programs/emacs/elisp/"
+                                      :base-directory "./home-manager/editor/emacs/elisp/"
                                       :base-extension "org"
                                       :exclude "README.org"
                                       :publishing-directory "./public")
                                      ("misc"
-                                      :base-directory "./home-manager/programs/emacs/misc/"
+                                      :base-directory "./home-manager/editor/emacs/misc/"
                                       :base-extension "org"
                                       :exclude "\\(README\\|mu4e-dashboard\\)\\.org"
                                       :publishing-directory "./public")
