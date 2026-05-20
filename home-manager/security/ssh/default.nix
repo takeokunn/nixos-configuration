eef626e0092f2f1ab116aedbf8bccd3fc35dfa42
takeokunn <bararararatty@gmail.com>
2026-05-20 11:23:08 +0900

    Update SSH config to use settings attribute with connection multiplexing

--- home-manager/security/ssh/default.nix
+++ home-manager/security/ssh/default.nix
@@ -2,6 +2,19 @@
   programs.ssh.enable = true;
   programs.ssh.enableDefaultConfig = false;
   programs.ssh.includes = [ "config.d/*" ];
-  programs.ssh.matchBlocks."*".compression = true;
-  programs.ssh.matchBlocks."*".serverAliveInterval = 15;
+  programs.ssh.settings."*".compression = true;
+  programs.ssh.settings."*".serverAliveInterval = 15;
+  programs.ssh.settings."*".serverAliveCountMax = 3;
+  programs.ssh.settings."*".connectTimeout = 10;
+  programs.ssh.settings."*".controlMaster = "auto";
+  programs.ssh.settings."*".controlPath = "~/.ssh/control/%C";
+  programs.ssh.settings."*".controlPersist = "10m";
+
+  home.activation.createSshControlDir = {
+    after = [ "writeBoundary" ];
+    before = [ ];
+    data = ''
+      install -d -m 0700 "$HOME/.ssh/control"
+    '';
+  };
 }
