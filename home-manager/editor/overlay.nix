7375e09d13640427a273a3ca9bb36f1095369aa5
takeo obara <bararararatty+github-actions-gpg@gmail.com>
2026-06-08 09:48:15 +0000

    Update lockfile

--- home-manager/editor/overlay.nix
+++ home-manager/editor/overlay.nix
@@ -1,10 +1,16 @@
 { emacs-overlay }:
 [
   (import emacs-overlay)
-  (_: prev: if prev.stdenv.isDarwin then {
-    emacs = prev.emacs.overrideAttrs (old: {
-      buildInputs = (old.buildInputs or [ ]) ++ [ prev.apple-sdk ];
-      NIX_CFLAGS_COMPILE = "-std=gnu11 -include stdbool.h";
-    });
-  } else { })
+  (
+    _: prev:
+    if prev.stdenv.isDarwin then
+      {
+        emacs = prev.emacs.overrideAttrs (old: {
+          buildInputs = (old.buildInputs or [ ]) ++ [ prev.apple-sdk ];
+          NIX_CFLAGS_COMPILE = "-std=gnu11 -include stdbool.h";
+        });
+      }
+    else
+      { }
+  )
 ]
