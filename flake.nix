1c1828983a6b72f35084c004216b3b97f5b71d34
takeokunn <bararararatty@gmail.com>
2026-07-05 02:18:15 +0900

    chore: use local nur-packages input

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
     zen-browser.inputs.home-manager.follows = "home-manager";
     firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
     firefox-addons.inputs.nixpkgs.follows = "nixpkgs";
-    nur-packages.url = "path:/Users/take/ghq/github.com/takeokunn/nur-packages";
+    nur-packages.url = "github:takeokunn/nur-packages";
     nur-packages.inputs.nixpkgs.follows = "nixpkgs";
     darwin-vz-nix.url = "github:takeokunn/darwin-vz-nix";
     darwin-vz-nix.inputs.nixpkgs.follows = "nixpkgs";
